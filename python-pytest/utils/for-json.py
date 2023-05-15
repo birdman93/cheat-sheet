79c082a0906cc529b270e1f9ba44d47121d847bd
Denis <47885411+birdman93@users.noreply.github.com>
2023-05-15 12:52:22 +0600

    .

--- python-pytest/utils/for-json.py
+++ python-pytest/utils/for-json.py
@@ -1,9 +1,9 @@
 import json
 
 
-def find_key(key: str = None, json_data: json = None) -> dict | None:
+def find_value_by_key(key: str = None, json_data: json = None) -> dict | None:
     """
-    Функция для рекурсивного поиска по ключу в json
+    Функция для рекурсивного поиска по ключу в JSON-объекте
 
     Возвращает:
     - Значение ключа, если он был найден
@@ -15,27 +15,57 @@ def find_key(key: str = None, json_data: json = None) -> dict | None:
             if k == key:
                 return v
             elif isinstance(v, (dict, list)):
-                result = find_key(key, v)
+                result = find_value_by_key(key, v)
                 if result is not None:
                     return result
 
     elif isinstance(json_data, list):
         for item in json_data:
-            result = find_key(key, item)
+            result = find_value_by_key(key, item)
             if result is not None:
                 return result
 
     return None
 
 
-def get_dict_with_value_from_json(json_obj: json = None, key: str = None, value: str = None) -> dict | None:
+def find_value_list_by_key(json_obj: json = None, key: str = None) -> list:
     """
-    Функция для рекурсивного поиска словаря, в котором находится целевое значение
+    Функция для рекурсивного поиска значений по ключу в JSON-объекте
 
     Возвращает:
-    - Словарь, в котором найдено значение.
+    - Список значений, соответствующих ключу, если найдены.
+    - Пустой список, если ключ не найден.
+    """
+
+    results = []
+
+    # Проверка, является ли json_obj словарем
+    if isinstance(json_obj, dict):
+        # Поиск ключа в текущем словаре
+        if key in json_obj:
+            results.append(json_obj[key])
+
+        # Поиск ключа во всех значениях словаря
+        for value in json_obj.values():
+            results.extend(find_value_list_by_key(value, key))
+
+    # Поиск ключа в списке
+    if isinstance(json_obj, list):
+        for item in json_obj:
+            results.extend(find_value_list_by_key(item, key))
+
+    return results
+
+
+def find_dict_with_value(json_obj: json = None, key: str = None, value: str = None) -> dict | None:
+    """
+    Функция для рекурсивного поиска словаря, в котором находится целевое значение, в JSON-объекте
+
+    Возвращает:
+    - Словарь с искомым значением
     - None, если значение не найдено или ключ не существует.
     """
+
     # Проверка, является ли json_obj словарем
     if isinstance(json_obj, dict):
         # Проверка наличия ключа в текущем словаре
@@ -44,14 +74,14 @@ def get_dict_with_value_from_json(json_obj: json = None, key: str = None, value:
 
         # Поиск значения во всех значениях словаря
         for val in json_obj.values():
-            result = get_dict_with_value_from_json(val, key, value)
+            result = find_dict_with_value(val, key, value)
             if result is not None:
                 return result
 
     # Поиск значения в списке
     if isinstance(json_obj, list):
         for item in json_obj:
-            result = get_dict_with_value_from_json(item, key, value)
+            result = find_dict_with_value(item, key, value)
             if result is not None:
                 return result
 
